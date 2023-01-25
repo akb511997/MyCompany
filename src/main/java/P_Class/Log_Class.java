3e36dba7a795dae66d6a1d6cc28b1447ce8da995
akb511997 <bolkarashvinikailas51@gmail.com>
2023-01-25 13:31:08 +0530

    massage

--- src/main/java/P_Class/Log_Class.java
+++ src/main/java/P_Class/Log_Class.java
@@ -2,4 +2,18 @@ package P_Class;
 
 public class Log_Class {
 
+	public static  void m1() {
+		
+	
+	int a=1;
+	int b=2;
+	int sum=a+b;
+	System.out.println(sum);
+	}
+	
+	public static void main(String[] args) {
+		m1();
+	}
+	
+	
 }
--- src/test/java/T_classes/log_T_class.java
+++ src/test/java/T_classes/log_T_class.java
@@ -1,5 +1,16 @@
 package T_classes;
 
 public class log_T_class {
+public static  void substraction() {
+	int a=5;
+	int b=2;
+	int substract=a-b;
+	System.out.println(substract);
+	
+}
 
+
+public static void main(String[] args) {
+	substraction();
+}
 }
